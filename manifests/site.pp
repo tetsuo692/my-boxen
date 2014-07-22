e7bbdae97cd937bfe3067053b448f9a53ecc2fc5
Guillaume BELLEGUIC <guillaume@belleguic.com>
2014-07-22 08:09:47 +0200

    set nodejs

--- manifests/site.pp
+++ manifests/site.pp
@@ -68,6 +68,10 @@ node default {
   include nodejs::v0_8
   include nodejs::v0_10
 
+  nodejs::module { 'coffee-script':
+    node_version => 'v0.10'
+  }
+  
   # default ruby versions
   ruby::version { '1.9.3': }
   ruby::version { '2.0.0': }
