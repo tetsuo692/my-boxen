ac597062477b0f376833acb2634a95d04f533a01
Guillaume BELLEGUIC <guillaume@belleguic.com>
2014-08-14 07:51:19 +0200

    some stuff

--- manifests/site.pp
+++ manifests/site.pp
@@ -71,7 +71,12 @@ node default {
   nodejs::module { 'coffee-script':
     node_version => 'v0.10'
   }
-  
+
+  class { 'nodejs::global':
+
+    version => 'v0.10'
+
+  }
   # default ruby versions
   ruby::version { '1.9.3': }
   ruby::version { '2.0.0': }
